01d386ea7d2cdf3b3afe3ce373e00925e28960cf
Николай Кирюхин <nkiruhin@yandex.ru>
2020-06-22 10:13:44 +0400

    About

--- VueForm.FrontEnd/src/views/About.vue
+++ VueForm.FrontEnd/src/views/About.vue
@@ -1,5 +1,11 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
+﻿<template>
+  <div style="margin:20px">
+    <h3 style="text-align: center;">О приложении</h3>
+    <p>Серверная часть:</p>
+    <ul>
+      <li>Основана на шаблоне приложения&nbsp;<a href="https://github.com/ardalis/CleanArchitecture">https://github.com/ardalis/CleanArchitecture.</a></li>
+      <li>Сущности находятся в проекте Core</li>
+      <li>Протестировать Api возможно через Swagger <a href="swagger">/swagger/</a></li>
+    </ul>
   </div>
 </template>
